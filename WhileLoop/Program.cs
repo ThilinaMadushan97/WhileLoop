91bcba7fe3341157195defa7dcca712fed2a3dac
dell <you@example.com>
2024-04-12 03:02:52 +0530

    Add project files.

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿Random randm = new Random();
+
+int i = randm.Next(1,10);
+Console.WriteLine($"First number is {i}");
+
+while (i != 7)
+{
+    Console.WriteLine(i);
+    i = randm.Next(1,10);
+}
+Console.WriteLine($"Current number is {i}");
